43c6043bf77e5956902ef8765062e4f5a48887c2
jstat17 <78304030+jstat17@users.noreply.github.com>
2021-03-25 22:17:36 +0200

    Update to 0.8.0, same as math contracts

--- contracts/RandomWalkNFT.sol
+++ contracts/RandomWalkNFT.sol
@@ -1,14 +1,16 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
 import "./NumericalMath.sol";
 import "./FixidityLib.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 
 /**
  * @title RandomWalkNFT
  * @author John Michael Statheros (GitHub: jstat17)
  * @notice Contract for creating random walk map NFTs.
  */
-contract RandomWalkNFT {
+contract RandomWalkNFT{
     uint256 id = 0;
     struct coords {
         int256[] x;
@@ -24,13 +26,14 @@ contract RandomWalkNFT {
     /**
      * @notice Generates a random int256 between some upper
      * and lower bounds using an int256 seed.
-     * @param seed: integer seed
+     * @param num_seed: integer seed
      * @param upper: upper-bound of the random number
      * @param lower: lower-bound of the random number
      * @return random int256 between upper and lower (inclusive)
      */
     function getRandomNum(int256 num_seed, int256 upper, int256 lower) public pure returns(int256) {
-        return rand_num = FixidityLib.abs(callKeccak256(abi.encodePacked(num_seed))) % (upper-lower+1) + lower;
+        int256 rand_num = FixidityLib.abs(callKeccak256(abi.encodePacked(num_seed))) % (upper-lower+1) + lower;
+        return rand_num;
     }
     
     /**
@@ -39,8 +42,27 @@ contract RandomWalkNFT {
      * @return int256 of hashed seed
      */
     function callKeccak256(bytes memory seed) internal pure returns(int256) {
-        return int(keccak256(seed));
+        return int256(uint256(keccak256(seed)));
     }
+    
+    //function bytes32ToInt256(bytes32 b) internal pure returns(int256) {
+        //int256 num;
+        //for(uint8 i = 0; i < 32; i++) {
+            //int256 exp = int256(2**uint256(8*(32-(i+1))));
+            //num = num + int256(uint8(bytes1(b[i])))*exp;
+        //}
+    //}
+    
+    //function bytes32ToInt256(bytes32 b) internal pure returns(int256) {
+        //bytes memory buffer = new bytes(32);
+        //buffer = b;
+        //bytes32 preI;
+        //uint256 offset = 32;
+        //assembly {
+            //preI := mload(add(b, offset))
+        //}
+        //int256 x = int256(preI);
+    //}
     
     function generateRandomWalk(int256 nodes) public returns(bool success) {
         return true;
